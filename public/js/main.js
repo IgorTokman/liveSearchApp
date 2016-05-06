2c40cf9cd0634d6ce995d776e69f1a7800c8c46d
IgorTokman <igortokman1994@ukr.net>
2016-05-06 18:23:41 +0300

    realized the installation jquery library by using bower. added some comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,22 @@
 (function () {
 
+    // Responds to input
     $('#search').keyup(function() {
+
+        // Keeps the author name
         var searchField = $('#search').val();
         var myExp = new RegExp(searchField, "i");
 
+        // Reads and parses data in the JSON format
         $.getJSON('data/data.json', function(data) {
+
             var output = '<ul class="searchresults">';
             $.each(data, function(key, val) {
 
+                //Checks if author exists
                 if (val.name.search(myExp) != -1) {
+
+                    // Generates the response records
                     output += '<li>';
                     output += '<h2>'+ val.name +'</h2>';
                     output += '<img src="images/'+ val.shortname +'_tn.jpg" alt="'+ val.name +'" />';
@@ -16,31 +24,17 @@
                     output += '</li>';
                 }
             });
+
             output += '</ul>';
+
+            // Fills the #update container
             $('#update').html(output);
-        }); //get JSON
+
+        }); // get JSON
     });
 
-    function  init() {
-
-        $.getJSON('data/data.json', function(data) {
-            var output = '<ul class="searchresults">';
-            $.each(data, function(key, val) {
-
-                    output += '<li>';
-                    output += '<h2>'+ val.name +'</h2>';
-                    output += '<img src="images/'+ val.shortname +'_tn.jpg" alt="'+ val.name +'" />';
-                    output += '<p>'+ val.bio +'</p>';
-                    output += '</li>';
-
-            });
-            output += '</ul>';
-            $('#update').html(output);
-        }); //get JSON
-    }
-
-    init();
-
+    //  It calls immediately
+    $('#search').keyup();
 
 })();
 
